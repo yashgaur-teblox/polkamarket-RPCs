30dc5f2fd0713bd1b322ce4de5966af8954a4c12
Wellington Matheus <w.matheus@outlook.com>
2021-08-09 13:18:44 -0300

    feat(Routes): create call route

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,10 @@
 import { Router } from 'express';
+import { contractController } from './useCases/Contract';
 
 const router = Router();
+
+router.get('/call', (request, response) => {
+  return contractController.handle(request, response);
+});
 
 export { router };
