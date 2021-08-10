e19354f675e1756dc8b114133cae6274c18994da
Wellington Matheus <w.matheus@outlook.com>
2021-08-10 08:21:42 -0300

    feat: configure cors and dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express from 'express';
+import * as dotenv from "dotenv";
+import cors from "cors";
+
 import { router } from './routes';
+
+dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
