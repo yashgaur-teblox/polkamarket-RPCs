bc723b0d99e94adc2c5d2547c3d8e3d8791e9072
Wellington Matheus <w.matheus@outlook.com>
2021-09-21 07:44:27 -0300

    feat(Events): create Schema

---
+++ src/useCases/Events/EventsSchema.ts
@@ -0,0 +1,9 @@
+import * as yup from 'yup';
+
+export const eventsSchema = yup.object({
+  contract: yup
+    .mixed()
+    .oneOf(['predictionMarket', 'erc20', 'realitio'])
+    .required('Contract is required!'),
+  method: yup.string().required('Method is required!')
+});
