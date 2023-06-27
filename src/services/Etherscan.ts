ceae4e452640a3ba675656fa246749925cc57594
Ricardo Marques <ricardo.ds.marques@gmail.com>
2023-06-27 10:35:04 -0600

    fix(Etherscan): re-add apiKey to request

--- src/services/Etherscan.ts
+++ src/services/Etherscan.ts
@@ -15,7 +15,7 @@ export class Etherscan {
   }
 
   public async getEvents(contract, address, fromBlock, toBlock, eventName, filter) {
-    let etherscanUrl = `${this.baseUrl}/api?module=logs&action=getLogs`;
+    let etherscanUrl = `${this.baseUrl}/api?module=logs&action=getLogs&apikey=${this.apiKey}`;
     etherscanUrl += `&address=${address}`;
     if (fromBlock) etherscanUrl += `&fromBlock=${fromBlock}`;
     if (toBlock) etherscanUrl += `&toBlock=${toBlock}`;
